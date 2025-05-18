03eead2db8cef2c0bc746fee44ff5d3062ca4e1b
PUITAR <puitar@foxmail.com>
2025-05-18 07:48:34 +0000

    camera ready

--- experiment/sp.py
+++ experiment/sp.py
@@ -23,99 +23,99 @@ num_threads_build = 48
 num_threads_search = 1
 perf_output = "perf/sp"
 
-system(f"""
-  cd .. && \
-  python create_dataset.py --dataset {dataset} \
-    --train_distr uniform --train_left 0 --train_right 1000 \
-    --test_distr uniform --test_left 0 --test_right 100 \
-    --k 100 --num_threads 48
-  """)
+# system(f"""
+#   cd .. && \
+#   python create_dataset.py --dataset {dataset} \
+#     --train_distr uniform --train_left 0 --train_right 1000 \
+#     --test_distr uniform --test_left 0 --test_right 100 \
+#     --k 100 --num_threads 48
+#   """)
 
-if not os.path.exists(perf_output):
-    os.makedirs(perf_output)
+# if not os.path.exists(perf_output):
+#     os.makedirs(perf_output)
 
-################################ Benchmark for PF-HNS and Hi-PNG ################################
-# Load dataset
-base_data = DataSet(os.path.join(data_directory, f"{dataset}.train.fvecs"))
-query_data = DataSet(os.path.join(data_directory, f"{dataset}.test.fvecs"))
-base_attrs = IntervalSet(os.path.join(data_directory, f"{dataset}.{distribution1}.train.itv"))
-query_attrs = IntervalSet(os.path.join(data_directory, f"{dataset}.{distribution2}.test.itv"))
-groundtruth = GroundTruth(os.path.join(data_directory, f"{dataset}.{distribution1}.{distribution2}.gt"))
+# ################################ Benchmark for PF-HNS and Hi-PNG ################################
+# # Load dataset
+# base_data = DataSet(os.path.join(data_directory, f"{dataset}.train.fvecs"))
+# query_data = DataSet(os.path.join(data_directory, f"{dataset}.test.fvecs"))
+# base_attrs = IntervalSet(os.path.join(data_directory, f"{dataset}.{distribution1}.train.itv"))
+# query_attrs = IntervalSet(os.path.join(data_directory, f"{dataset}.{distribution2}.test.itv"))
+# groundtruth = GroundTruth(os.path.join(data_directory, f"{dataset}.{distribution1}.{distribution2}.gt"))
 
-index_type = "hnsw"
+# index_type = "hnsw"
 
-index_param_1, index_param_2 = [32, 128], [10000, 32, 128]
-search_params_1, search_params_2 = [[ef, iter] for ef in range(10, 200, 20) for iter in range(1, 10, 2)], \
-    [[ef, iter, sim/100] for ef in range(10, 30, 4) for iter in range(1, 8, 2) for sim in range(0, 101, 20)]
+# index_param_1, index_param_2 = [32, 128], [10000, 32, 128]
+# search_params_1, search_params_2 = [[ef, iter] for ef in range(10, 200, 20) for iter in range(1, 10, 2)], \
+#     [[ef, iter, sim/100] for ef in range(10, 30, 4) for iter in range(1, 8, 2) for sim in range(0, 101, 20)]
 
-timer = Timer()
+# timer = Timer()
 
-# Benchmark for PostFilterGraph
-model = PostFilterGraph(index_type, index_param_1, "euclidean")
-timer.reset()
-timer.start()
-model.build(base_data, base_attrs, num_threads_build)
-timer.stop()
-build_time_sec = timer.get()
-model.get_comparison_and_clear()
-performance = []
-for search_param in tqdm(search_params_1):
-    timer.reset()
-    timer.start()
-    _, knn = model.search(query_data, query_attrs, topk, search_param, num_threads_search)
-    timer.stop()
-    search_time_sec = timer.get()
-    comparison = model.get_comparison_and_clear()
-    performance.append({
-        "name": "HNSW",
-        "index_param": index_param_1,
-        "index_size": model.index_size(),
-        "build_time": build_time_sec,
-        "k": topk,
-        "search_param": search_param,
-        "search_time": search_time_sec,
-        "qps": query_data.size() / search_time_sec,
-        "comparison": comparison,
-        "cps": comparison / search_time_sec,
-        "recall": groundtruth.recall(topk, knn)})
-    # Save results
-    json.dump(performance, open(os.path.join(perf_output, f"{dataset}.HNSW.json"), 'w') ,indent=2)
+# # Benchmark for PostFilterGraph
+# model = PostFilterGraph(index_type, index_param_1, "euclidean")
+# timer.reset()
+# timer.start()
+# model.build(base_data, base_attrs, num_threads_build)
+# timer.stop()
+# build_time_sec = timer.get()
+# model.get_comparison_and_clear()
+# performance = []
+# for search_param in tqdm(search_params_1):
+#     timer.reset()
+#     timer.start()
+#     _, knn = model.search(query_data, query_attrs, topk, search_param, num_threads_search)
+#     timer.stop()
+#     search_time_sec = timer.get()
+#     comparison = model.get_comparison_and_clear()
+#     performance.append({
+#         "name": "HNSW",
+#         "index_param": index_param_1,
+#         "index_size": model.index_size(),
+#         "build_time": build_time_sec,
+#         "k": topk,
+#         "search_param": search_param,
+#         "search_time": search_time_sec,
+#         "qps": query_data.size() / search_time_sec,
+#         "comparison": comparison,
+#         "cps": comparison / search_time_sec,
+#         "recall": groundtruth.recall(topk, knn)})
+#     # Save results
+#     json.dump(performance, open(os.path.join(perf_output, f"{dataset}.HNSW.json"), 'w') ,indent=2)
     
-# Benchmark for Hi-PNG
-model = HiPNG(index_type, index_param_2, "euclidean")
-timer.reset()
-timer.start()
-model.build(base_data, base_attrs, num_threads_build)
-timer.stop()
-build_time_sec = timer.get()
-model.get_comparison_and_clear()
-performance = []
-for search_param in tqdm(search_params_2):
-    timer.reset()
-    timer.start()
-    _, knn = model.search(query_data, query_attrs, topk, search_param, num_threads_search)
-    timer.stop()
-    search_time_sec = timer.get()
-    comparison = model.get_comparison_and_clear()
-    performance.append({
-        "name": "Hi-PNG-HNSW",
-        "index_param": index_param_2,
-        "index_size": model.index_size(),
-        "build_time": build_time_sec,
-        "k": topk,
-        "search_param": search_param,
-        "search_time": search_time_sec,
-        "qps": query_data.size() / search_time_sec,
-        "comparison": comparison,
-        "cps": comparison / search_time_sec,
-        "recall": groundtruth.recall(topk, knn)})
-    # Save results
-    json.dump(performance, open(os.path.join(perf_output, f"{dataset}.Hi-PNG-HNSW.json"), 'w'), indent=2)
+# # Benchmark for Hi-PNG
+# model = HiPNG(index_type, index_param_2, "euclidean")
+# timer.reset()
+# timer.start()
+# model.build(base_data, base_attrs, num_threads_build)
+# timer.stop()
+# build_time_sec = timer.get()
+# model.get_comparison_and_clear()
+# performance = []
+# for search_param in tqdm(search_params_2):
+#     timer.reset()
+#     timer.start()
+#     _, knn = model.search(query_data, query_attrs, topk, search_param, num_threads_search)
+#     timer.stop()
+#     search_time_sec = timer.get()
+#     comparison = model.get_comparison_and_clear()
+#     performance.append({
+#         "name": "Hi-PNG-HNSW",
+#         "index_param": index_param_2,
+#         "index_size": model.index_size(),
+#         "build_time": build_time_sec,
+#         "k": topk,
+#         "search_param": search_param,
+#         "search_time": search_time_sec,
+#         "qps": query_data.size() / search_time_sec,
+#         "comparison": comparison,
+#         "cps": comparison / search_time_sec,
+#         "recall": groundtruth.recall(topk, knn)})
+#     # Save results
+#     json.dump(performance, open(os.path.join(perf_output, f"{dataset}.Hi-PNG-HNSW.json"), 'w'), indent=2)
 
 ################################ Benchmark for QuadTree ################################
 
 base_data = fvecs_read(os.path.join(data_directory, f"{dataset}.train.fvecs"))
-query_data = fvecs_read(os.path.join(data_directory, f"{dataset}.test.fvecs"))
+query_data = fvecs_read(os.path.join(data_directory, f"{dataset}.test.fvecs"))[:100]
 base_attrs = fvecs_read(os.path.join(data_directory, f"{dataset}.{distribution1}.train.itv"))
 query_attrs = fvecs_read(os.path.join(data_directory, f"{dataset}.{distribution2}.test.itv"))
 
@@ -124,9 +124,7 @@ groundtruth = GroundTruth(os.path.join(data_directory, f"{dataset}.{distribution
 imin, imax = base_attrs.min(), base_attrs.max()
 
 timer = Timer()
-
-# initialize ray
-ray.init(num_cpus=num_threads_search)
+timer_filter = Timer()
 
 # Build QuadTree index
 timer.start()
@@ -136,45 +134,39 @@ for i, (l, r) in enumerate(base_attrs):
 timer.stop()
 build_time_sec = timer.get()
 
-@ray.remote
 def batch_query(queries, attrs, topk, spindex, base_data, base_attrs):
     results = []
-    for q, attr in zip(queries, attrs):
-        region = (attr[0], attr[0], attr[1], attr[1])
+    for q, (l, r) in zip(queries, attrs):
+        timer_filter.start()
+        region = (l, l, r, r)
         inter = spindex.intersect(region)
-        idx = np.array([id for id in inter if (base_attrs[id][0] >= attr[0] and base_attrs[id][1] <= attr[1])])
+        idx = np.array([id for id in inter if (base_attrs[id][0] >= l and base_attrs[id][1] <= r)])
+        timer_filter.stop()
         knn = []
         if len(idx) > 0:
             X = base_data[idx]
             dists = np.sum((X - q)**2, axis=1)
             actual_topk = min(len(dists), topk)
-            topk_idx = np.argpartition(dists, actual_topk-1)[:actual_topk]  # Select indices with smallest distances
+            topk_idx = np.argpartition(dists, actual_topk-1)[:actual_topk]
             knn = idx[topk_idx].tolist() 
         knn = knn + [0x3f3f3f3f] * max(0, topk-len(knn))
         results.append(knn)
     return results
 
-# Split the queries and attributes into num_threads chunks
-chunk_size = (len(query_data) + num_threads_search - 1) // num_threads_search
-query_chunks = [query_data[i:i+chunk_size] for i in range(0, len(query_data), chunk_size)]
-attr_chunks = [query_attrs[i:i+chunk_size] for i in range(0, len(query_attrs), chunk_size)]
-
-spindex = ray.put(spindex)
-base_data = ray.put(base_data)
-base_attrs = ray.put(base_attrs)
-
 timer.reset()
-# Process chunks in parallel
+# Process all queries in single thread
+print(158)
 timer.start()
-result_chunks = ray.get([
-    batch_query.remote(q_chunk, a_chunk, topk, spindex, base_data, base_attrs)
-    for q_chunk, a_chunk in zip(query_chunks, attr_chunks)
-])
+results = batch_query(query_data, query_attrs, topk, spindex, base_data, base_attrs)
 timer.stop()
 search_time_sec = timer.get()
+search_time_filter = timer_filter.get()
+
+print(f"filter time: {search_time_filter}")
+print(f"search time: {search_time_sec}")
 
 # Combine results
-knn = [n for chunk in result_chunks for knn in chunk for n in knn]
+knn = [n for knn in results for n in knn]
 recall = groundtruth.recall(topk, knn)
 qps = len(query_data) / search_time_sec
 
@@ -189,11 +181,9 @@ performance = {
 }
 json.dump([performance], open(os.path.join(perf_output, f"{dataset}.QuadTree.json"), 'w'), indent=2)
 
-ray.shutdown()
-
 ################################ Benchmark for R-Tree ################################
 base_data = fvecs_read(os.path.join(data_directory, f"{dataset}.train.fvecs"))
-query_data = fvecs_read(os.path.join(data_directory, f"{dataset}.test.fvecs"))
+query_data = fvecs_read(os.path.join(data_directory, f"{dataset}.test.fvecs"))[:100]
 base_attrs = fvecs_read(os.path.join(data_directory, f"{dataset}.{distribution1}.train.itv"))
 query_attrs = fvecs_read(os.path.join(data_directory, f"{dataset}.{distribution2}.test.itv"))
 groundtruth = GroundTruth(os.path.join(data_directory, f"{dataset}.{distribution1}.{distribution2}.gt"))
@@ -211,12 +201,15 @@ for i, (l, r) in enumerate(base_attrs):
 
 timer.stop()
 build_time_sec = timer.get()
+timer_filter = Timer()
 
 def batch_query(queries, attrs, topk, rt_index, base_data, base_attrs):
     results = []
     for q, (l,r) in zip(queries, attrs):
+        timer_filter.start()
         inter = list(rt_index.intersection((l, l, r, r), objects=False))
         idx = np.array([id for id in inter if (base_attrs[id][0] >= l and base_attrs[id][1] <= r)])
+        timer_filter.stop()
         knn = []
         if len(idx) > 0:
             X = base_data[idx]
@@ -228,12 +221,16 @@ def batch_query(queries, attrs, topk, rt_index, base_data, base_attrs):
         results.append(knn)
     return results
 
+print(224)
 timer.reset()
 # Process all queries in single thread
 timer.start()
 results = batch_query(query_data, query_attrs, topk, rt_index, base_data, base_attrs)
 timer.stop()
 search_time_sec = timer.get()
+search_time_filter = timer_filter.get()
+print(f"filter time: {search_time_filter}")
+print(f"search time: {search_time_sec}")
 
 # Combine results
 knn = [n for knn in results for n in knn]
